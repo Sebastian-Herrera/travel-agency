71a7acbbd88c13927d23b1ce6162068b26d1d42b
Sebastian Herrera <herrera6911sebastian@hotmail.com>
2021-07-22 22:11:36 -0500

    opciones reales

--- src/views/Home/HomeForm/HomeForm.jsx
+++ src/views/Home/HomeForm/HomeForm.jsx
@@ -1,16 +1,23 @@
 import HomeOption from '../HomeOption/HomeOption';
 
+// https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/CO/COP/es-CO/BOG-sky/LHR-sky/anytime
+
 const HomeForm = () => {
-  const data = ['cop', 'usd'];
+  const pais = ['CO', 'usd'];
+  const moneda = ['COP', 'USD'];
+  const origen = ['es-CO', 'usd'];
+  const from = ['BOG-sky', 'usd'];
+  const to = ['LHR-sky', 'usd'];
+  const tiempo = ['anytime', 'usd'];
 
   return (
     <form>
-      <HomeOption title='from' data={data} />
-      <HomeOption title='to' data={data} />
-      <HomeOption title='departure' data={data} />
-      <HomeOption title='return' data={data} />
-      <HomeOption title='cabin' data={data} />
-      <HomeOption title='passengers' data={data} />
+      <HomeOption title='from' data={pais} />
+      <HomeOption title='to' data={moneda} />
+      <HomeOption title='departure' data={origen} />
+      <HomeOption title='return' data={from} />
+      <HomeOption title='cabin' data={to} />
+      <HomeOption title='passengers' data={tiempo} />
     </form>
   );
 };
